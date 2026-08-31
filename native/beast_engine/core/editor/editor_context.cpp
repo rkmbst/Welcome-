7de8eba3aa3e090975056e377c03e4df8d15afa0
rkmbst <khaouni0rabah13@gmail.com>
2026-08-31 17:14:43 +0100

    Update editor_context.cpp

--- native/beast_engine/core/editor/editor_context.cpp
+++ native/beast_engine/core/editor/editor_context.cpp
@@ -1,21 +1,55 @@
-#include "editor_context.h"
-
-#include "../state/editor_state.h"
+#include "editor.h"
 
 namespace beast {
 
-EditorContext::EditorContext(
-    EditorState& state
-) noexcept
-    : state_(state) {
+Editor::Editor(
+    std::unique_ptr<Project> project
+)
+    : state_(std::move(project)),
+      context_(state_) {
 }
 
-EditorState& EditorContext::state() noexcept {
-    return state_;
+CommandResult Editor::execute(
+    std::unique_ptr<Command> command
+) {
+    const CommandResult result =
+        history_.execute(
+            std::move(command),
+            context_
+        );
+
+    if (result == CommandResult::Success) {
+        state_.bumpRevision();
+    }
+
+    return result;
 }
 
-const EditorState& EditorContext::state() const noexcept {
-    return state_;
+CommandResult Editor::undo() {
+    const CommandResult result =
+        history_.undo(context_);
+
+    if (result == CommandResult::Success) {
+        state_.bumpRevision();
+    }
+
+    return result;
+}
+
+CommandResult Editor::redo() {
+    const CommandResult result =
+        history_.redo(context_);
+
+    if (result == CommandResult::Success) {
+        state_.bumpRevision();
+    }
+
+    return result;
+}
+
+std::unique_ptr<Snapshot>
+Editor::createSnapshot() const {
+    return state_.createSnapshot();
 }
 
 } // namespace beast
