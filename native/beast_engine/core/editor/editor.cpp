7e9605be8eea840843bf3990be495549d167bbc1
rkmbst <khaouni0rabah13@gmail.com>
2026-08-31 17:29:06 +0100

    Create editor.cpp

---
+++ native/beast_engine/core/editor/editor.cpp
@@ -0,0 +1,59 @@
+#include "editor.h"
+
+namespace beast {
+
+Editor::Editor(
+    std::unique_ptr<Project> project
+)
+    : state_(
+        std::make_unique<Document>(
+            std::move(project)
+        )
+      ),
+      context_(state_) {
+}
+
+CommandResult Editor::execute(
+    std::unique_ptr<Command> command
+) {
+    const CommandResult result =
+        history_.execute(
+            std::move(command),
+            context_
+        );
+
+    if (result == CommandResult::Success) {
+        state_.bumpRevision();
+    }
+
+    return result;
+}
+
+CommandResult Editor::undo() {
+    const CommandResult result =
+        history_.undo(context_);
+
+    if (result == CommandResult::Success) {
+        state_.bumpRevision();
+    }
+
+    return result;
+}
+
+CommandResult Editor::redo() {
+    const CommandResult result =
+        history_.redo(context_);
+
+    if (result == CommandResult::Success) {
+        state_.bumpRevision();
+    }
+
+    return result;
+}
+
+std::unique_ptr<Snapshot>
+Editor::createSnapshot() const {
+    return state_.createSnapshot();
+}
+
+} // namespace beast
